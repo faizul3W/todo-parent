fb4c24a0959fbe33142ba288eda3f9ea94775400
faizul3w <faizul@triplewsols.org>
2023-02-19 18:01:07 +0600

    first setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useRef, useEffect, useState } from "react";
 
 function App() {
+  const iFrameRef = useRef(null);
+  const [recievedMessage, setRecievedMessage] = useState("");
+
+  const sendMessage = () => {
+    if (!iFrameRef.current) return;
+    iFrameRef.current.contentWindow.postMessage(
+      "Hello son!",
+      "http://localhost:3000"
+    );
+  };
+
+  useEffect(() => {
+    window.addEventListener("message", function (e) {
+      if (e.origin !== "http://localhost:3000") return;
+      setRecievedMessage("Got this message from child: " + e.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Parent iFrame</h1>
+      <button
+        onClick={() => {
+          sendMessage();
+        }}
+      >
+        Send message to child
+      </button>
+
+      <iframe
+        ref={iFrameRef}
+        src="http://localhost:3000/"
+        width="600"
+        height="300"
+        title="Child iframe"
+      ></iframe>
+
+      <p>{recievedMessage}</p>
     </div>
   );
 }
